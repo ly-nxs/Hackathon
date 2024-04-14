622d2358a3718e49cd00b9194bc21ab3e9f56067
ly-nxs <6hurgusburgus9@gmail.com>
2024-04-14 16:17:08 -0700

    renable more maps

--- game/client/src/maps/mapList.js
+++ game/client/src/maps/mapList.js
@@ -56,21 +56,21 @@ export class MapList {
         this.addMap("city3", [{next: "ruins", coords:[12,12,12,24]}])
         this.addMap("bossroomsmallfit", [{next: "cave2", coords:[16,19,30,2]}])
 
-        // this.addMap("startingareafit", [ {next: "startinghouse", coords: [10,9,7,14] },{next: "cavefit", coords: [42,8,3,7] }]);
-        // this.addMap("startinghouse", [{next: "startingareafit", coords: [5,15,10,12]}]);
+        this.addMap("startingareafit", [ {next: "startinghouse", coords: [10,9,7,14] },{next: "cavefit", coords: [42,8,3,7] }]);
+        this.addMap("startinghouse", [{next: "startingareafit", coords: [5,15,10,12]}]);
 
-        // this.addMap("cavefit", [{next: "startingareafit", coords: [1,7,42,7]}, {next: "town2smallfit", coords:[42,3,4,18]}])
+        this.addMap("cavefit", [{next: "startingareafit", coords: [1,7,42,7]}, {next: "town2smallfit", coords:[42,3,4,18]}])
         
-        // this.addMap("town2smallfit", [{next: "cavefit", coords: [3,18,42,4]}, {next: "ruinsmallfit", coords: [32,5,8,4]},
-        //  {next: "generichouse", coords:[8,10,10,11]}, 
-        // {next: "town2house2", coords:[18,10,10,11]}])
-        // this.addMap("generichouse", [{next: "town2smallfit", coords:[11,11,8,11]}])
-        // this.addMap("town2house2",  [{next: "town2smallfit", coords:[11,11,18,11]}])
+        this.addMap("town2smallfit", [{next: "cavefit", coords: [3,18,42,4]}, {next: "ruinsmallfit", coords: [32,5,8,4]},
+        {next: "generichouse", coords:[8,10,10,11]}, 
+        {next: "town2house2", coords:[18,10,10,11]}])
+        this.addMap("generichouse", [{next: "town2smallfit", coords:[11,11,8,11]}])
+        this.addMap("town2house2",  [{next: "town2smallfit", coords:[11,11,18,11]}])
         
-        // this.addMap("ruinsmallfit", [{next: "town2smallfit", coords:[8,3,32,5]}, {next: "bossroomsmallfit", coords:[30,2,16,18]}])
+        this.addMap("ruinsmallfit", [{next: "town2smallfit", coords:[8,3,32,5]}, {next: "bossroomsmallfit", coords:[30,2,16,18]}])
 
-        // this.addMap("bossroomsmallfit", [{next:"ruinsmallfit", coords:[16,19,30,2]}])
-        // // console.log(this.maps, "MAPS")
+        this.addMap("bossroomsmallfit", [{next:"ruinsmallfit", coords:[16,19,30,2]}])
+        //console.log(this.maps, "MAPS")
 
         // this.addNpcs("startingarea", [{npc: new NPC("Billy.png", 448,528)}])
         this.addNpcs("startingarea", [{npc: trainer}])
