c0d5add5bf3bba0e00c9b5ee7dadb1876f0cf0ae
jacob52571 <lijacob290@gmail.com>
2024-04-14 17:20:32 -0700

    cleaned up some stuff

--- app/api/submitQuestion/route.ts
+++ app/api/submitQuestion/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 import { db } from "../../dbSetup";
 
-export async function POST(req: Request){
+// function to submit the player's answer to a question
+export async function POST(req: Request) {
     try {
+        // get vars from the request body
         const body = await req.json();
         const playerId = body.playerId;
         const questionId = body.questionId;
@@ -19,17 +21,20 @@ export async function POST(req: Request){
             });
         });
 
+        // check to see if the player exists
         if (!row) {
             return NextResponse.json({ error: "Player not found" }, { status: 404 });
         }
 
+        // get the player's category and current streak
         const category: string = (row as { category: string }).category;
         const currentStreak: number = (row as { streak: number }).streak;
+        // get the correct answer from the answers file
         const fs = require('fs');
         const answers = fs.readFileSync(`./app/questions/${category}-answers.txt`, 'utf8').toLowerCase().split('\n');
         const correctAnswer = answers[questionId];
+        // update the streak based on the answer
         let newStreak = 0;
-        console.log(answer, correctAnswer);
         if (answer == correctAnswer) {
             newStreak = currentStreak + 1;
         }
@@ -45,8 +50,10 @@ export async function POST(req: Request){
             });
         });
 
+        // return if the answer was correct, the new streak, and the correct answer
         return NextResponse.json({ correct: answer == correctAnswer, streak: newStreak, correctAnswer: correctAnswer });
     }
+    // catch errors
     catch (error) {
         console.log("API ERROR", error);
         return NextResponse.json({ error: "API ERROR" }, { status: 500 })
